81a6a48d5dbbd5b184957a53e91477bec797250f
tulios <tulio@corp.globo.com>
2012-10-05 14:36:23 -0300

    draft of plugin and tests

--- spec/javascripts/responsivehub_spec.js
+++ spec/javascripts/responsivehub_spec.js
@@ -1,5 +1,97 @@
 describe("ResponsiveHub", function() {
-  it("should do nothing", function() {
-    expect(true).toBeTruthy();
+
+  describe("init", function() {
+
+     it("should bind 'resize' to '_updateLayout'", function() {
+       var win = jasmine.createSpyObj("win", ["bind"]);
+       spyOn(win, "bind");
+       spyOn($.responsiveHub("self"), "_getWindow").andReturn(win);
+       expect(win.bind).toHaveBeenCalledWith("resize", $.responsiveHub("self")._updateLayout);
+       $.responsiveHub({layouts: {960: "web"}, default: "web"});
+     });
+
+     it("should disable resize bound", function() {
+     });
+
+     it("should detect media query support", function() {
+     });
+
+     it("should calculate current layout", function() {
+     });
+
   });
+
+  describe("layout", function() {
+
+    beforeEach(function() {
+      $.responsiveHub({
+        layouts: {
+          320: "phone",
+          960: "web",
+          768: "tablet"
+        },
+        default: "web"
+      });
+
+    });
+
+    it("should return matched layout", function() {
+      spyOn($.responsiveHub("self"), "width").andReturn(800);
+      expect($.responsiveHub("layout")).toEqual("tablet");
+    });
+
+    it("should return 'default' if does not support media queries", function() {
+      spyOn($.responsiveHub("self"), "width").andReturn(100);
+      $.responsiveHub("self").hasMediaQuerySupport = false;
+      expect($.responsiveHub("layout")).toEqual("web");
+    });
+
+  });
+
+  describe("width", function() {
+
+    it("should retrieve the window width", function() {
+      var win = jasmine.createSpyObj("win", ["width"]);
+      $.responsiveHub("self").windowObj = win;
+
+      $.responsiveHub("width");
+      expect(win.width).toHaveBeenCalled();
+    });
+
+  });
+
+  describe("isTouch", function() {
+
+    it("should be true if ontouchstart exist", function() {
+      var win = jasmine.createSpyObj("win", ["ontouchstart"]);
+      $.responsiveHub("self").windowObj = win;
+      expect($.responsiveHub("isTouch")).toEqual(true);
+    });
+
+    describe("when dealing with DocumentTouch", function() {
+      var originalDocumentTouch;
+      beforeEach(function() {
+        originalDocumentTouch = window.DocumentTouch;
+      });
+
+      afterEach(function() {
+        window.DocumentTouch = originalDocumentTouch;
+      });
+
+      it("should be true if document is an instance of DocumentTouch", function() {
+        var DocumentTouch = function() {};
+        window.DocumentTouch = DocumentTouch;
+        var win = {
+          DocumentTouch: DocumentTouch,
+          document: new DocumentTouch()
+        };
+        $.responsiveHub("self").windowObj = win;
+        expect($.responsiveHub("isTouch")).toEqual(true);
+      });
+
+    });
+
+  });
+
+  
 });
---
+++ src/responsive_hub.js
@@ -0,0 +1,119 @@
+/*!
+ * ResponsiveHub - JavaScript goodies for Responsive Design
+ * https://github.com/globocom/responsive-hub
+ * version: 0.1.0
+ */
+
+;(function ($, window, document) {
+
+  $.responsiveHub = function(settings) {
+    if (typeof settings === "object") {
+      ResponsiveHub.init(settings);
+
+    } else if (typeof settings === "string") {
+      var args = [].splice.call(arguments,0);
+      var methodName = args.splice(0, 1)[0];
+      if (ResponsiveHub[methodName]) {
+        return ResponsiveHub[methodName].apply(ResponsiveHub, args);
+      } else {
+        throw "[ResponsiveHub] Undefined method '" + methodName + "'";
+      }
+    }
+  }
+
+  var ResponsiveHub = {
+    NAMESPACE: "ResponsiveHub_",
+    NAMESPACE_READY: "ResponsiveHubReady_",
+
+    currentLayout: null,
+    resizeBound: false,
+    hasMediaQuerySupport: false,
+    windowObj: null,
+
+    init: function(settings) {
+      this.windowObj = this._getWindow();
+      this.layouts = settings.layouts;
+      this.default = settings.default;
+      this._boot();
+    },
+
+    self: function() {
+      return this;
+    },
+
+    width: function() {
+      return this.windowObj.width();
+    },
+
+    layout: function() {
+      if (!this.hasMediaQuerySupport) {
+        return this.default;
+      }
+
+      var widths = this._keys(this.layouts);
+      widths.sort();
+      widths.reverse();
+
+      var width = this.width();
+      for (var i in widths) {
+        var w = widths[i];
+        if (width > w) return this.layouts[w];
+      }
+    },
+
+    isTouch: function() {
+      return !!(('ontouchstart' in this.windowObj) || (this.windowObj.DocumentTouch && this.windowObj.document instanceof DocumentTouch));
+    },
+
+    hasFlash: function() {
+      try { return !! new ActiveXObject('ShockwaveFlash.ShockwaveFlash'); } catch(e1) {}
+      var mimeType = this._mimeTypeFlash();
+      return !! (mimeType && mimeType.enabledPlugin);
+    },
+
+    _updateLayout: function() {
+    },
+
+    _boot: function() {
+      if (!this.resizeBound) {
+        this.windowObj.bind("resize", this._updateLayout);
+        this.resizeBound = true;
+        this.hasMediaQuerySupport = Modernizr.mq("only all");
+      }
+
+      if (!this.currentLayout) {
+        this.currentLayout = this.layout();
+      }
+
+      var readyEvent = this.NAMESPACE_READY + this.currentLayout;
+      windowObj.trigger(readyEvent, [{
+        layout: this.currentLayout,
+        touch: this.isTouch()
+      }]);
+
+      windowObj.unbind(readyEvent);
+    },
+
+    _getWindow: function() {
+      return $(window);
+    },
+
+    _mimeTypeFlash: function() {
+      return navigator.mimeTypes["application/x-shockwave-flash"];
+    },
+
+    // https://github.com/jiujitsumind/underscorejs/blob/master/underscore.js#L644
+    _keys: Object.keys || function(obj) {
+      if (obj !== Object(obj)) throw new TypeError('Invalid object');
+      var keys = [];
+      for (var key in obj) if (obj.hasOwnProperty(key)) keys[keys.length] = key;
+      return keys;
+    },
+
+    _isArray: Array.isArray || function(obj) {
+      return Object.prototype.toString.call(obj) === '[object Array]';
+    }
+
+  };
+
+ })(jQuery, window, document);
